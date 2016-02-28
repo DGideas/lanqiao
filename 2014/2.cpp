ce6db4675baa165fd52d71daf51a2cfee65c237c
DGideas <dgideas@outlook.com>
2016-02-28 12:30:58 +0800

    Update 2.cpp

--- 2014/2.cpp
+++ 2014/2.cpp
@@ -1,1 +1,31 @@
-//一根高筋拉面，中间切一刀，可以得到2根面tiao条
+//一根高筋拉面，中间切一刀，可以得到2根面条
+#include <iostream>
+#include <cctype>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+        int L1 = 1;
+        int L2 = 0;
+        int R = 1;
+        int n;
+        cin>>n;
+        if (n == 0)
+        {
+                cout<<"2"<<endl;
+                return 0;
+        }
+        for (int i=0; i<n; i++)
+        {
+                L1 += L2;
+                L2 = R;
+                R = pow(2, i);
+        }
+        cout<<L1 + L2 + R<<endl;
+        return 0;
+}
